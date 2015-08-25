3c7be63fb0641ac7fafa41691d8f3b2bd0fcf0b4
cghil <cghilarducci@gmail.com>
2015-08-24 18:04:23 -0700

    todoApp first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var todoApp = angular.module('todo', ['ngResources', 'ngMockE2E']);
+
+weatherApp.config(function($routeProvider){
+	
+	$routeProvider
+
+	.when('/', {
+		templateUrl:
+	})
+
+	.when('/todos', {
+		templateUrl: 
+	})
+})
