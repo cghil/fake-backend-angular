24943d59481ab1c1e695f3fb4e7477620e0fb768
cghil <cghilarducci@gmail.com>
2015-08-28 22:15:15 -0700

    added functionality to add a todo to list

--- backend.js
+++ backend.js
@@ -4,10 +4,18 @@ todoApp.run(function($httpBackend){
 
 	// $httpBackend.whenGET('/todos').respond(todos);
 
-	var todos = [{description: 'get milk', status: 'completed'}, {description: 'laundry needs to be folded', status: 'incomplete'}, {description: 'clean the kitchen', status: 'completed'}];
+	todos = [{id: 1, description: 'get milk', status: 'completed'}, {id: 2, description: 'laundry needs to be folded', status: 'incomplete'}, {id: 3, description: 'clean the kitchen', status: 'completed'}];
 
     //returns the current list of todos
     $httpBackend.whenGET('/todos').respond(todos);
+
+    $httpBackend.whenPOST('/todos').respond(function(method, url, data){
+    	var todo = angular.fromJson(data);
+    	var id = todos.length + 1;
+    	todo.id = id;
+    	todos.unshift(todo);
+    	return [200, todos];
+    });
 
     $httpBackend.whenGET('pages/home.html').passThrough();
     $httpBackend.whenGET('pages/todos.html').passThrough();
--- controllers/todoscontroller.js
+++ controllers/todoscontroller.js
@@ -1,25 +1,39 @@
 todoApp.controller('todosController', ['$scope', '$log', '$http', function($scope, $log, $http) {
 
-	// $http.get('/todos')
-	// 	.then(function(response){
-	// 		$scope.todos = response;
-	// 		$log.log('todos have been found')
-	// 	}, function(response){
-	// 		$log.log('unable to find todos, server error')
-	// 	});
-
 	$scope.newTodo = '';
+
+	// $scope.$watch('newTodo', function(){
+	// 	$log.info($scope.newTodo);
+	// })
 	
 	$http.get('/todos')
         .success(function(response){
             $scope.todos = response;
-            console.log(response);
         })
         .error(function(data, status){
             console.log(data);
         });
 
     $scope.addATodo = function(){
+    	
+    	var date = new Date();
+    	var day = date.getDate();
+    	var month = date.getMonth();
+    	var year = date.getFullYear();
+
+    	var date = month + "/" + day + "/" + year;
+
+    	$http.post('/todos', {description: $scope.newTodo, status: "incomplete", date: date })
+    		.success(function(response){
+    			
+    			$scope.todos = response;
+    			$scope.newTodo = '';
+
+    		}).error(function(data, status){
+
+    			console.log(data);
+
+    		})
 
     }
 
