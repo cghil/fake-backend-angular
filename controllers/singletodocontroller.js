83ba982775479d15e8e501cb1b40e4576b52edee
cghil <cghilarducci@gmail.com>
2015-09-03 18:44:21 -0700

    User can edit a todo

--- controllers/singletodocontroller.js
+++ controllers/singletodocontroller.js
@@ -4,10 +4,27 @@ todoApp.controller('showATodoController', ['$scope', '$log', '$http', '$routePar
 
 	var number = params.id;
 
-
 	$http.get('/todos/'+number)
 		.then(function(response){
 			$scope.todo = response.data;
-		})
+		});
+
+	$scope.submitEdit = function(){
+		console.log('I am the number... ' + number)
+		$http.put('/todos/'+number, { description: $scope.todo.description, status: $scope.todo.status, date: $scope.todo.date, id: number })
+			.success(function(reply){
+
+				var message = "Todo got updated!";
+
+			});
+		$scope.isFormShowing = false;
+	}
+
+	$scope.isFormShowing = false;
+
+	$scope.showEditForm = function(){
+		var toggle = $scope.isFormShowing;
+		$scope.isFormShowing = !toggle;
+	};
 
 }]);
